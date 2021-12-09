7f5540ae56a745accb7ef51c204ccd99d1083bab
Abhishek <info_tech@smartbits.co.in>
2021-12-09 18:37:47 +0530

    Offcanvas menu and header

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import Header from '../src/components/header';
-import Footer from '../src/components/footer';
+// import Footer from '../src/components/footer';
 
 let App = () => {
   const element = (
